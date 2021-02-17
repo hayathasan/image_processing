fe15dd8f6494f5f7f103f2a9cf80cece4ec7423c
Hayat Hasan <hayat.hasan@gmail.com>
2021-02-17 10:29:32 +0600

    ..

--- index.php
+++ index.php
@@ -1,17 +1,63 @@
+<?php
+    $template_dir = "template";
+    if(!is_dir($template_dir)){
+        mkdir($template_dir);
+    }
+
+    $templates = array_values(array_diff(scandir($template_dir), [".",".."]));
+        
+    if(isset($_GET['template']) && $_GET['template'] != ''){
+        // $template = json_decode(file_get_contents($template_dir."/".$_GET['template']),true);
+        $template = file_get_contents($template_dir."/".$_GET['template']);
+        echo $template; 
+        exit();        
+    }
+    
+    // foreach ($template as $key => $name) {
+    //     ${$key} = $name; 
+    // }
+    // echo "<pre>"; print_r($template); echo "</pre>";
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Image Processing</title>
-    <style>
-        body {
-            padding: 10px;
-            padding-right: 20px;
-        }
-    </style>
+    <script src="jquery.min.js"></script>
 </head>
 <body>
+    <label for="selectTemplate">Source Template:</label>
+    <select name="selectedTemplate" id="selectTemplate">
+        <option value=""></option>
+        <?php 
+            foreach ($templates as $key => $value) {
+                echo "<option value='$value'>$value</option>";
+            }
+        ?>
+    </select>
 
-    <form action="image.php" method="POST">
+    <script>
+        $(document).ready(function(){
+            $("#selectTemplate").change(function() {
+                var template_name = $("#selectTemplate option:selected").val();
+                $.get('index.php?template='+template_name, function(data){
+                    data = $.parseJSON(data);
+                    console.log(data);
+                    $("#templateForm input").each(function(){
+                        // console.log($(this).attr("name"));
+
+                        $(this).val( data[$(this).attr("name")] );
+                    })
+
+
+                });
+            });
+
+        })
+    </script>
+
+    <form id="templateForm" action="image.php" method="POST">
     
         <h1>Source & Destination:</h1>
         
@@ -24,13 +70,13 @@
         <br><br>
 
         <label for="">Image Height:</label>
-        <input type="text" name="image_height" value="600">
+        <input type="text" name="image_height" value="">
 
         <label for="">Image Width:</label>
-        <input type="text" name="image_width" value="600">
+        <input type="text" name="image_width" value="">
 
         <label for="">Blur Background:</label>
-        <input type="checkbox" name="bg_blur">        
+        <input type="checkbox" name="bg_blur" value="">        
         <br><br>
 
         <h1>Logo:</h1>
@@ -46,14 +92,14 @@
         <input type="text" name="logo_y" value="">
 
         <label for="">Logo Position (Center):</label>
-        <input type="checkbox" name="logo_center">
+        <input type="checkbox" name="logo_center" value="">
 
         <br><br>
 
 
         <h1>Text:</h1>
         <label for="">Add Text:</label>
-        <input type="checkbox" name="isAddText"> <br>
+        <input type="checkbox" name="isAddText" value=""> <br>
 
 
         <label for="">Text1:</label>
